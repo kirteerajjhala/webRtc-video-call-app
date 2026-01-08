b0c0aee07bad981c6eb1408020b196b00be0c5e3
Kirteeraj jhala <kirteerajjhala@gmail.com>
2026-01-08 17:56:00 +0530

    some error slove

--- Server.js
+++ Server.js
@@ -1,25 +1,25 @@
 const express = require("express")
 const http = require("http")
 const { Server } = require("socket.io")
+const path = require("path")
 
 const app = express()
 const server = http.createServer(app)
 const io = new Server(server)
 
-app.use(express.static(__dirname))
+// Serve static files from 'public' folder
+app.use(express.static(path.join(__dirname, "public")))
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html")
+// All routes fallback to index.html (for single-page apps)
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"))
 })
 
 io.on("connection", (socket) => {
-  console.log("User connected:", socket.id)
-
   socket.on("offer", (offer) => socket.broadcast.emit("offer", offer))
   socket.on("answer", (answer) => socket.broadcast.emit("answer", answer))
   socket.on("candidate", (candidate) => socket.broadcast.emit("candidate", candidate))
 })
 
-server.listen(3000, () => {
-  console.log("Server running at http://localhost:3000")
-})
+const PORT = process.env.PORT || 3000
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
